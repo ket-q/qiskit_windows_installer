9d80b636b06ca9dd49203b528d1c83a62721d20b
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-03-16 15:59:49 +0900

    removed since private

--- dummy.ps1
+++
@@ -1,9 +0,0 @@
-#
-# Empty script used for testing download and invocation methods.
-#
-
-# Stop the script when a cmdlet or a native command fails
-$ErrorActionPreference = 'Stop'
-$PSNativeCommandUseErrorActionPreference = $true
-
-Write-Host 'Script is executing ...'
