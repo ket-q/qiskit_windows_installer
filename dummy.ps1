76e119670d7dfa59fcca608153311c40ef9ff9e3
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-03-16 15:53:57 +0900

    added

---
+++ dummy.ps1
@@ -0,0 +1,9 @@
+#
+# Empty script used for testing download and invocation methods.
+#
+
+# Stop the script when a cmdlet or a native command fails
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+Write-Host 'Script is executing ...'
